d0b8648ab312ef2cbc37c95f26b701cc60b19fd9
shizuka-virus <muhammadfarooq1085@gmail.com>
2025-10-30 20:10:31 +0500

    Add implementation of bitwise OR operation method

---
+++ B_bit_magic/B_operation_or.py
@@ -0,0 +1,9 @@
+def method1(n: int, m: int) -> bin:
+    return bin(n | m)
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    print(timeit(lambda: method1(25, 30), number=10000)) # 0.0016012300000056712
+    """
