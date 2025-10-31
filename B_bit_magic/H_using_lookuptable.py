e1d131b646e34d48dd399df5e44a32076d9ca53d
shizuka-virus <muhammadfarooq1085@gmail.com>
2025-10-31 20:56:58 +0500

    Add implementation of method to count set bits using a lookup table

---
+++ B_bit_magic/H_using_lookuptable.py
@@ -0,0 +1,21 @@
+bitsettable = [0] * 256
+bitsettable[0] = 0
+
+for i in range(256):
+    bitsettable[i] = (i & 1) + bitsettable[i // 2]
+
+
+def method1(n: int) -> int:
+    return (
+        bitsettable[n & 0xFF]
+        + bitsettable[(n >> 8) & 0xFF]
+        + bitsettable[(n >> 16) & 0xFF]
+        + bitsettable[(n >> 24 & 0xFF)]
+    )
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    print(timeit(lambda: method1(9), number=10000)) # 0.003812235000623332
+    """
