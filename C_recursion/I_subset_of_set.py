827cb52e35e657a87439f5818e08b90ce5d0e4d9
shizuka-virus <muhammadfarooq1085@gmail.com>
2025-10-31 20:57:54 +0500

    Add implementation of methods to generate subsets of a set

---
+++ C_recursion/I_subset_of_set.py
@@ -0,0 +1,19 @@
+def method1(n: list) -> list:
+    x = len(n)
+    masks = [1 << i for i in range(x)]
+    for i in range(1 << x):
+        yield [ss for mask, ss in zip(masks, n) if i & mask]
+
+
+def method2(n: set) -> list:
+    from itertools import combinations
+
+    return [x for i in range(len(n) + 1) for x in combinations(n, i)]
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    print(timeit(lambda: method1([1, 2, 3, 4, 5, 6]), number=10000)) # 0.003230058999179164
+    print(timeit(lambda: method2({1, 2, 3, 4, 5, 6}), number=10000)) # 0.055786498000088613
+    """
