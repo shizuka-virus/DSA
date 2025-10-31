03fb127ef69389af535e4cae3cd9456e828aae98
shizuka-virus <muhammadfarooq1085@gmail.com>
2025-10-31 20:57:08 +0500

    Add implementation of recursive factorial method

---
+++ C_recursion/A_factorial.py
@@ -0,0 +1,9 @@
+def method1(n: int) -> int:
+    return 1 if n == 0 or n == 1 else n * method1(n - 1)
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    print(timeit(lambda: method1(10), number=10000)) # 0.012313627999901655
+    """
