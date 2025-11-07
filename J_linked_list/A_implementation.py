a2bb0a59c10ae68ceb2251855ff992a96cb9f561
shizuka-virus <muhammadfarooq1085@gmail.com>
2025-11-07 22:16:53 +0500

    Add initial implementation of Node and LinkedList classes

---
+++ J_linked_list/A_implementation.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    Llist = LinkedList()
+    Llist.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+    second.next = third
+    print(timeit(lambda: Llist, number=10000)) # 0.00037003099987487076
+    """
