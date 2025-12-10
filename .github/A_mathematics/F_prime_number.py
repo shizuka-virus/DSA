fa1753ba8c79ab33d952ad2ac76031eca946ea43
MUHAMMAD FAROOQ <161140921+shizuka-virus@users.noreply.github.com>
2025-12-10 18:04:03 +0000

    Add F_prime_number.py with methods to check for prime numbers using range and itertools

---
+++ .github/A_mathematics/F_prime_number.py
@@ -0,0 +1,28 @@
+def method1(n: int) -> bool:
+    '''Return a boolean value to check if 
+    there exists any factors greater than 1
+    by using range()
+    '''
+
+    return n > 1 and all(n % i for i in range(2, int(n ** 0.5) + 1))
+
+
+def method2(n: int) -> bool:
+    '''Return a boolean value to check if 
+    there exists any factors greater than 1
+    by using islice() and count() methods of itertools
+    '''
+    from itertools import count, islice
+
+    def prime(n: int) -> bool:
+        return n > 1 and all(n % i for i in islice(count(2), int((n) ** 0.5 - 1)))
+
+    return prime(n)
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    print(timeit(lambda: method1(10), number=10000)) # 0.006183429999509826
+    print(timeit(lambda: method2(10), number=10000)) # 0.015483204999327427
+    """
